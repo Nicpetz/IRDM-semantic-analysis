998c93437c51195f0afb95dfc9eb5036b7c328f6
Alex Lilburn <alexlilburn@me.com>
2016-03-07 10:42:01 +0000

    Add function for retrieving list of data file directories

--- Util/Import.py
+++ Util/Import.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import os
 
 
 def load_file(filename):
@@ -14,9 +15,30 @@ def load_file(filename):
     except:
         raise ValueError('Filename could not be processed. '
                          'Check it is a path to .txt file of Twitter data in json format')
+
     with open(filename) as f:
         data = json.load(f)
+
     data = pd.DataFrame(data['tweets'])
     data['date'] = data['date'].map(lambda x: int(x/1000))
     data['date'] = pd.to_datetime(data['date'], unit='s')
+
     return data
+
+
+def get_files(data_directory='./data'):
+    """
+    Function for getting list of text files
+    :param data_directory: path to directory of data
+    :return: list of directories
+    """
+    dir_list = []
+
+    if not data_directory.endswith('/'):
+        data_directory += '/'
+
+    for file in os.listdir(data_directory):
+        if file.endswith(".txt"):
+            dir_list.append(data_directory+file)
+
+    return dir_list
