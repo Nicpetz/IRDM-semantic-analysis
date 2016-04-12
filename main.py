40dc586abdd395fe0f96a1ff362fb6846678e7ab
Alex Lilburn <alexlilburn@me.com>
2016-04-12 13:17:50 +0100

    set file ready to run

--- main.py
+++ main.py
@@ -6,37 +6,37 @@ from Util.Import import load_new_file, get_files
 from nnmf import *
 import json
 
-import regex
-import re
-import pandas as pd
-from emoji import UNICODE_EMOJI
-import unicodedata as ud
 
-paths = get_files('./data/')
-length = len(paths)
+number_of_files = 5
+number_of_topics = 5
+iterations = 20
+matrix_density = 0.1
 
-with open('id_to_term_dictionary.txt', 'r') as f:
-    dict = json.load(f)
 
-unique_terms = len(dict.keys())
+if __name__ == "__main__":
+    paths = get_files('./data/')
+    length = len(paths)
 
-# Uncomment to run factorisation on entire dataset
-# matrix = []
-# for i, path in enumerate(paths):
-#     print("Loading matrix: {0:0.2f}%".format((i / length) * 100), end='\r')
-#     data = load_new_file(path)
-#     matrix += data['vector'].tolist()
-# print("Matrix loaded.")
-# del data
+    with open('id_to_term_dictionary.txt', 'r') as f:
+        dict = json.load(f)
 
-matrix = load_new_file(paths[0])
-matrix = matrix['vector'].tolist()
+    unique_terms = len(dict.keys())
 
-matrix = build_sparse_matrix(matrix, unique_terms, verbose=True)
+    # Uncomment to run factorisation on entire dataset
+    matrix = []
+    for i, path in enumerate(paths[:number_of_files]):
+        print("Loading matrix: {0:0.2f}%".format((i / length) * 100), end='\r')
+        data = load_new_file(path)
+        matrix += data['vector'].tolist()
+    print("Matrix loaded.")
+    del data
 
-w, h = factorise(matrix, topics=5, iterations=20, init_density=0.1)
+    # matrix = load_new_file(paths[0])
+    # matrix = matrix['vector'].tolist()
 
-print("Successfuly factorised!")
+    matrix = build_sparse_matrix(matrix, unique_terms, verbose=True)
 
-evaluate(w, dict)
+    w, h = factorise(matrix, topics=number_of_topics, iterations=iterations, init_density=matrix_density)
+
+    evaluate(w, dict)
 
